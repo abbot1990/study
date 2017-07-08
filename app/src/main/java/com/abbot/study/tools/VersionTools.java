bea4e66bd51e8b72ebf8ff9d81e61c49f7e072c7
fangzheng <185849480@qq.com>
2017-07-08 20:30:10 +0800

    no message

---
+++ app/src/main/java/com/abbot/study/tools/VersionTools.java
@@ -0,0 +1,18 @@
+package com.abbot.study.tools;
+
+import android.os.Build;
+import android.support.compat.BuildConfig;
+
+/**
+ * <pre>
+ *     author : fangzheng
+ *     e-mail : 185849480@qq.com
+ *     time   : 2017/07/08
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class VersionTools {
+
+
+}
